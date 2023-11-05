091d5b1fd0962d93c20242898b0acba14ee557f7
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-05 18:11:00 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/Components/DefaultUI/Navbar";
 import Footer from "@/Components/DefaultUI/Footer";
+import Providers from "@/Components/Providers/Providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -77,7 +78,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }

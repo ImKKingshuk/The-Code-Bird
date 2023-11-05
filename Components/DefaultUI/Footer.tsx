83032c775802808bca1738b764f0f3461afe67f8
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-05 17:28:24 +0530

    updated

---
+++ Components/DefaultUI/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface FooterProps {}
+
+function Footer({}: FooterProps) {
+  return (
+    <div>
+      <h1>hi</h1>
+    </div>
+  );
+}
+
+export default Footer;

7261bc7bfe35629aaecc9a76f78bbb7124ad9eee
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-07 07:14:35 +0530

    updated

--- Components/DefaultUI/Footer.tsx
+++ Components/DefaultUI/Footer.tsx
@@ -17,6 +17,15 @@ function Footer({}: FooterProps) {
                 <p className="text-2xl p-1 font-bold">The Code Bird</p>
               </div>
             </Link>
+
+            <p className="text-justify">
+              "The Code Bird" is the biggest coding club of UIT, where budding
+              tech enthusiasts flock together to spread their wings and explore
+              the fascinating world of programming. With engaging workshops,
+              collaborative projects, and a supportive community, we nurture
+              creativity, problem-solving, and a passion for coding. Join us and
+              soar to new coding heights!
+            </p>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <div>

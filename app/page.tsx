8444ca963a0785d53f410025381393530f8a9811
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2024-01-31 18:05:41 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-blue-500 text-5xl">
+      <h1 className="text-blue-500 text-5xl text-center">
         The Code Bird, Tailwind is working
       </h1>
     </div>

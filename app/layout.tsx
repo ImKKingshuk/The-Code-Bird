f9f22ea637139a3c7967359689268da666caa1c0
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2024-01-31 18:04:04 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,7 +75,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+          <Navbar />
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }

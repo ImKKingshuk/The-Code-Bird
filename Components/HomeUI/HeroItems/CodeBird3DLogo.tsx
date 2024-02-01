a09e56f9688bf9c72f32352a0e026d2b528634b7
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2024-02-01 20:11:22 +0530

    updated

--- Components/HomeUI/HeroItems/CodeBird3DLogo.tsx
+++ Components/HomeUI/HeroItems/CodeBird3DLogo.tsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import { Canvas, useLoader } from "@react-three/fiber";
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader";
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
 import { PerspectiveCamera, OrbitControls, Stats } from "@react-three/drei";
 import { Suspense } from "react";
 
@@ -8,11 +8,11 @@ interface ModelProps {
   nodes: { Scene: THREE.Group };
 }
 
-const Model: React.FC<ModelProps> = ({ nodes }) => {
+function Model({ nodes }: ModelProps) {
   const gltf = useRef<THREE.Group>();
 
   return <primitive ref={gltf} object={nodes.Scene} />;
-};
+}
 
 interface CodeBird3DLogoProps {}
 

742fd902935bafd60186662c203d68bb27010311
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2024-02-01 19:58:52 +0530

    updated

---
+++ Components/HomeUI/HeroItems/CodeBird3DLogo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface CodeBird3DLogoProps {}
+
+function CodeBird3DLogo({}: CodeBird3DLogoProps) {
+  return <div>3DLogo</div>;
+}
+
+export default CodeBird3DLogo;

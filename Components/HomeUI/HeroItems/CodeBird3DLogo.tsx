205e7b7845d77696b2325f846b7c1718960e7525
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2024-02-01 21:07:53 +0530

    updated

--- Components/HomeUI/HeroItems/CodeBird3DLogo.tsx
+++ Components/HomeUI/HeroItems/CodeBird3DLogo.tsx
@@ -20,15 +20,15 @@ function CodeBird3DLogo({}: CodeBird3DLogoProps) {
   return (
     <div className="w-full h-full">
       <Canvas>
-        <PerspectiveCamera makeDefault position={[0, 0, 10]} />
+        <PerspectiveCamera makeDefault position={[0, 0, 5]} />
         <Suspense fallback={null}>
           <Model {...useLoader(GLTFLoader, "/CodeBird3D.glb")} />
         </Suspense>
         <OrbitControls
           enableZoom
           zoomSpeed={0.5}
-          minPolarAngle={Math.PI / 4}
-          maxPolarAngle={Math.PI / 1.5}
+          minPolarAngle={Math.PI / 10}
+          maxPolarAngle={Math.PI / 2}
         />
         {/* <Stats /> */}
       </Canvas>
